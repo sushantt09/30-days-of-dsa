d088b292dc1d002101ae3ad3430bd5445692051d
Sushant Sharma <127682674+sushantt09@users.noreply.github.com>
2025-06-30 16:30:21 +0530

    some changes

--- day25/solution.js
+++ day25/solution.js
@@ -43,3 +43,49 @@ MyStack.prototype.top = function() {
 MyStack.prototype.empty = function() {
     return this.q1.length === 0;
 };
+
+// problem 2 : implement a queue using two stacks
+
+var MyQueue = function() {
+    this.s1 = [];
+    this.s2 = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.s1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if (this.s2.length === 0) {
+        while (this.s1.length > 0) {
+            this.s2.push(this.s1.pop());
+        }
+    } 
+    return this.s2.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    if (this.s2.length === 0) {
+        while (this.s1.length > 0) {
+            this.s2.push(this.s1.pop());
+        }
+    }
+    return this.s2[this.s2.length -1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.s2.length === 0 && this.s1.length === 0;
+};
